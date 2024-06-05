1b502935305a3f50961c8c15c0301ffc6e9ef018
ageltorm <ageltorm@gmail.com>
2024-06-05 14:51:38 +0300

    use docker util in excute endpoint

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from app.docker_utils import execute_code_in_docker
 from app.schemas import TaskRequest, TaskResponse
 
 app = FastAPI()
@@ -6,7 +7,7 @@ app = FastAPI()
 @app.post("/execute", response_model=TaskResponse)
 async def execute_task(task_request: TaskRequest):
     try:
-        result = "success"
+        result = await execute_code_in_docker(task_request)
         return TaskResponse(output=result)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
